1793f247bd86852f16ac261a61fb58cbbf62bd54
Aleh Yaromenka <aleh.yaromenka@insurity.com>
2023-01-22 19:32:08 +0300

    feature: add draw actions

--- src/action-controller.ts
+++ src/action-controller.ts
@@ -1,5 +1,6 @@
 import {Command} from "./command-resolver";
 import {getMousePosition, moveMouseDown, moveMouseLeft, moveMouseRight, moveMouseUp} from "./mouse-control-actions";
+import {drawCircle, drawRectangle} from "./draw-actions";
 
 enum Commands {
     MOUSE_POSITION = 'mouse_position',
@@ -7,15 +8,24 @@ enum Commands {
     MOUSE_DOWN = 'mouse_down',
     MOUSE_LEFT = 'mouse_left',
     MOUSE_RIGHT = 'mouse_right',
+    DRAW_CIRCLE = 'draw_circle',
+    DRAW_RECTANGLE = 'draw_rectangle',
+    DRAW_SQUARE = 'draw_square',
+    PRINT_SCREEN = 'prnt_scrn',
 }
 
 const getFirstParamAsNumber = (command: Command): number => {
-    return command ? Number(command.params[0]) : 0;
+    return command && command.params.length > 0 ? Number(command.params[0]) : 0;
+}
+
+const getSecondParamAsNumber = (command: Command): number => {
+    return command && command.params.length > 1 ? Number(command.params[1]) : 0;
 }
 
 const doAction = async (command: Command): Promise<string> => {
     let response: string = command.cmd;
     const firstParam = getFirstParamAsNumber(command);
+    const secondParam = getFirstParamAsNumber(command);
 
     switch (command.cmd) {
         case Commands.MOUSE_POSITION:
@@ -32,6 +42,16 @@ const doAction = async (command: Command): Promise<string> => {
             break;
         case Commands.MOUSE_RIGHT:
             await moveMouseRight(firstParam);
+            break;
+
+        case Commands.DRAW_CIRCLE:
+            await drawCircle(firstParam);
+            break;
+        case Commands.DRAW_RECTANGLE:
+            await drawRectangle(firstParam, secondParam);
+            break;
+        case Commands.DRAW_SQUARE:
+            await drawRectangle(firstParam, firstParam);
             break;
     }
 
